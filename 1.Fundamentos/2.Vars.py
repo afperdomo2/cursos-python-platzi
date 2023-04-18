d13736423376fb7ca0d4827eede03f59085e0c10
alvarado0925 <alvarado0925@gmail.com>
2023-04-18 00:12:19 -0500

    🧪Feat (1.Fundamentos): Usando variables

---
+++ 1.Fundamentos/2.Vars.py
@@ -0,0 +1,13 @@
+# Variables
+
+first_name = input("➡️  Ingresa tu nombre: ")
+last_name = input("➡️  Ingresa tu apellido: ")
+age = input("➡️  Ingresa tu edad: ")
+nationality = input("➡️  Ingresa tu Nacionalidad: ")
+
+print(f"""
+Mi nombre es {first_name} {last_name}
+y tengo {age} años
+""")
+
+print("Nacionalidad:", nationality)
