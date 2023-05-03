0cf8955934ce4d2b87fc2f85ed0c49f2799b2455
alvarado0925 <alvarado0925@gmail.com>
2023-05-02 23:54:22 -0500

    🧪Feat (1.Fundamentos): Usando tipos de datos

---
+++ 1.Fundamentos/3.Types.py
@@ -0,0 +1,50 @@
+# Tipos de datos
+
+# Strings - Cadenas de texto
+first_name = "Felipe"
+print("first_name =>", first_name)
+print("type:", type(first_name))
+
+
+# int - Enteros
+age = 27
+print("\nage =>", age)
+print("type:", type(age))
+
+# floats - Flotantes: Representan números reales con decimales
+value = 18.9
+print("\nvalue =>", value)
+print("type:", type(value))
+
+
+# Boolean - Booleanos: Representan valores de verdadero o falso
+is_single = True
+print("\nis_single =>", is_single)
+print("type:", type(is_single))
+
+
+# Lists - Listas
+notes = [10, 4, 20]
+print("\nnotes =>", notes)
+print("type:", type(notes))
+
+# Tuple - Tuplas: Representan una secuencia inmutable de elementos
+colors = ("Red", "Green", "Yellow")
+print("\ncolors =>", colors)
+print("type:", type(colors))
+
+# Set - Conjuntos: Representan una colección desordenada y sin elementos repetidos
+animals = {"dog", "cat", "bird"}
+print("\nanimals =>", animals)
+print("type:", type(animals))
+
+# Dic - Diccionarios: Representan una colección de pares clave-valor
+people = {'nombre': 'Juan', 'edad': 30, 'ciudad': 'Bogotá'}
+print("\npeople =>", people)
+print("type:", type(people))
+
+
+# inputs - Tipos de entrada
+my_age = input("\n➡️  ¿Cuál es tu edad?:")
+print("my_age =>", my_age)
+print("type:", type(my_age))
