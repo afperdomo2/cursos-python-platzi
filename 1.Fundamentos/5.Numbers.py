388f790c460255811c98becba680d8895bf7166f
alvarado0925 <alvarado0925@gmail.com>
2023-05-03 22:56:51 -0500

    🧪Feat (1.Fundamentos): Usando Numbers

---
+++ 1.Fundamentos/5.Numbers.py
@@ -0,0 +1,11 @@
+# Numbers
+
+lives = 3
+lives -= 1  # Restar 1 vida
+print("lives =>", lives)
+print("type:", type(lives))
+
+temp = 34.8
+temp += 2  # Sumar a la variable
+print("\ntemp =>", temp)
+print("type:", type(temp))
