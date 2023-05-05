87802113d64a874530415697ff222b8c4c0a74b8
alvarado0925 <alvarado0925@gmail.com>
2023-05-04 23:56:06 -0500

    🧪Feat (1.Funadmentos): Realizando transformación de tipos

---
+++ 1.Fundamentos/7.Transformation.py
@@ -0,0 +1,22 @@
+# Transformación de tipos
+
+name = "Felipe"
+print("type:", type(name))
+
+name = 12
+print("type:", type(name))
+
+name = True
+print("type:", type(name))
+
+
+print("Andrés" + "Perdomo")
+print(10 + 30)
+
+age = 30
+print("Mi edad es " + str(age))
+print(f"Mi edad es {age}")  # Formatea a string la variable
+
+# Formatear directamente desde el input
+age = int(input("➡️  Ingresa tu edad:"))
+print("tipe:", type(age))
