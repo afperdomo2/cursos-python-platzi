5db39cb01fba67641b5e2582c11b44d06a75b81c
alvarado0925 <alvarado0925@gmail.com>
2023-05-08 21:58:53 -0500

    🧪Feat (1.Fundamentos): Usando operadores aritméticos

---
+++ 1.Fundamentos/8.Arith.py
@@ -0,0 +1,13 @@
+# Opradores aritméticos
+
+print(10 + 10)  # 20
+print(5 - 2)  # 3
+print(8 / 2)  # 4.0
+print(8 // 2)  # 4 => // Solo captura el número entero de la división
+print(7 * 3)  # 21
+print(10 % 2)  # 0
+print(2 ** 3)  # 8 => ** Elevar un valor
+print(2 ** 3 + 3 - 7 / 1 // 4)  # 10.0
+
+print("hola" + "mundo")  # Concatenar 2 cadenas
+print("🍷Salud" * 3)  # Multiplicar un string
