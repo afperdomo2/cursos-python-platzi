d80c5af4c709c36a8bee57e875b117fe431dc92f
alvarado0925 <alvarado0925@gmail.com>
2023-05-15 22:40:10 -0500

    🧪Feat (1.Fundamentos): Usando operadores de comparación

---
+++ 1.Fundamentos/9.Comparators.py
@@ -0,0 +1,33 @@
+# Operadores de comparación
+
+# Mayor que
+print(7 > 3)  # True
+print(3 > 7)  # False
+print(7 > 7)  # False
+
+# Menor que
+print(5 < 3)  # False
+print(3 < 5)  # True
+print(3 < 3)  # False
+
+# Mayor o igual
+print(5 >= 3)  # True
+print(5 >= 3)  # True
+print(5 >= 10)  # False
+
+# Menor o igual
+print(10 <= 21)  # True
+print(10 <= 5)  # False
+
+# Igual a
+print(5 == 5)  # True
+print(5 == 7)  # False
+print("hola" == "hola")  # True
+print("hola" == "adios")  # False
+
+print(5 == "5")  # False
+print(5 == int("5"))  # True
+
+# Diferente de
+print(5 != 5)  # False
+print(1 != 9)  # True
