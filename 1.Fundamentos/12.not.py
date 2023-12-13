8c5a36e629d565ad42c126ff7b69eaa1268420ab
alvarado0925 <alvarado0925@gmail.com>
2023-12-12 23:47:22 -0500

    🧪Feat (1.Fundamentos): Usando el operador de negación not

---
+++ 1.Fundamentos/12.not.py
@@ -0,0 +1,14 @@
+# El operador not en Python se utiliza para negar
+# una expresión booleana. Cuando se aplica el operador
+# not a una expresión booleana, el resultado será el
+# valor opuesto al valor original de la expresión.
+
+print(not True)
+print(not False)
+
+# 1. and
+print("\nNOT AND")
+print("not True and True:", not (True and True))  # False
+print("not True and False:", not (True and False))  # True
+print("not False and True:", not (False and True))  # True
+print("not False and False:", not (False and False))  # True
