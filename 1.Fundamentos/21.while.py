0ed720111114d13b232b853623237ba13115d05c
alvarado0925 <alvarado0925@gmail.com>
2023-12-18 14:44:56 -0500

    🧪Feat (1.Fundamentos): Usando el ciclo While

---
+++ 1.Fundamentos/21.while.py
@@ -0,0 +1,24 @@
+if True:
+    print("Se cumple la condición")
+
+counter = 0
+print("counter:", counter)
+while counter < 3:
+    print(counter)
+    counter += 1
+
+x = 0
+print("x:", x)
+while x < 10:
+    print(x)
+    if x == 5:
+        break
+    x += 1
+
+y = 0
+print("y:", y)
+while y < 10:
+    y += 1
+    if y < 7:
+        continue  # Salta a la siguiente iteración
+    print(y)
