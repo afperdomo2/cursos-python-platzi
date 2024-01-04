243f72912db106587ef5f4d69f33e7abb674ee4e
alvarado0925 <alvarado0925@gmail.com>
2024-01-04 00:00:56 -0500

    🧪Feat (2.Comprehensions): Usando funciones

---
+++ 2.Comprehensions/07_func.py
@@ -0,0 +1,17 @@
+print("hola mundo")
+
+
+def my_print(text):
+    print(text * 2)
+
+
+my_print("hola mundo")
+my_print("hola a todos")
+
+
+def summar(a, b):
+    my_print(a + b)
+
+
+summar(2, 4)
+summar(50, 7)
