c34019af96fd78cb6f637a7c3d598116d4ba571d
alvarado0925 <alvarado0925@gmail.com>
2024-01-09 17:02:51 -0500

    🚀Refactor (2.Comprehensions): Refactoriza el juego de piedra, papel o tijera

--- 2.Comprehensions/11_game.py
+++ 2.Comprehensions/11_game.py
@@ -1,21 +1,10 @@
 import random
 
 options = ("piedra", "papel", "tijera")
-
-user_wins = 0
-computer_wins = 0
-
 rounds_to_win = 3
-rounds = 1
 
-while True:
-    print("*" * 10)
-    print("ROUND", rounds)
-    print("*" * 10)
 
-    print("🧙user wins:", user_wins)
-    print("🤖computer wins:", computer_wins)
-
+def choose_options():
     while True:
         user_option = input("🕹️ ¿Piedra, papel o tijera?:").lower()
         if user_option in options:
@@ -23,7 +12,10 @@ while True:
         print("🚫 Opción inválida")
 
     computer_option = random.choice(options)
+    return user_option, computer_option
 
+
+def check_rules(user_option, computer_option, user_wins, computer_wins):
     print("Jugando...")
     msg = f"🧙 {user_option.capitalize()} vs {computer_option.capitalize()} 🤖"
     print(msg)
@@ -41,13 +33,38 @@ while True:
         computer_wins += 1
         print("❌ Perdiste!")
 
-    # Partida finalizada
-    if user_wins == rounds_to_win or computer_wins == rounds_to_win:
-        print("\nPartida finalizada:")
-        if user_wins == rounds_to_win:
-            print("🎉🎉🎉 Ganaste la partida!")
-        else:
-            print("👎👎👎 Perdiste la partida!")
-        break
-    rounds += 1
-    print("\n")
+    return user_wins, computer_wins
+
+
+def start_game():
+    user_wins = 0
+    computer_wins = 0
+    rounds = 1
+
+    while True:
+        print("*" * 10)
+        print("ROUND", rounds)
+        print("*" * 10)
+
+        print("🧙user wins:", user_wins)
+        print("🤖computer wins:", computer_wins)
+
+        user_option, computer_option = choose_options()
+
+        user_wins, computer_wins = check_rules(
+            user_option, computer_option, user_wins, computer_wins
+        )
+
+        # Partida finalizada
+        if user_wins == rounds_to_win or computer_wins == rounds_to_win:
+            print("\nPartida finalizada:")
+            if user_wins == rounds_to_win:
+                print("🎉🎉🎉 Ganaste la partida!")
+            else:
+                print("👎👎👎 Perdiste la partida!")
+            break
+        rounds += 1
+        print("\n")
+
+
+start_game()
