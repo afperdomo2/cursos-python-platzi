0cd6786b63b8f82b35f29d9563657034fd4cb65e
alvarado0925 <alvarado0925@gmail.com>
2024-05-13 19:56:26 -0500

    🧪Feat (2.Comprehensions): Usando módulos como scripts

--- 2.Comprehensions/app/main.py
+++ 2.Comprehensions/app/main.py
@@ -1,10 +1,17 @@
 import utils
 
-keys, values = utils.get_population()
 
-print("countries:", keys)
-print("values:", values)
+def run():
+    keys, values = utils.get_population()
 
-country = input("Enter a country: ")
-population = utils.population_by_country(country)
-print(f"Population of {country}: {population}")
+    print("countries:", keys)
+    print("values:", values)
+
+    country = input("Enter a country: ")
+    population = utils.population_by_country(country)
+    print(f"Population of {country}: {population}")
+
+
+# Esta condición se utiliza para que el código solo se ejecute si se ejecuta el archivo main.py
+if __name__ == "__main__":
+    run()
