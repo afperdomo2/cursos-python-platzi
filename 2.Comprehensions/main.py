4817ab0c4ca68710064a57cc389cce08f27e2fa2
alvarado0925 <alvarado0925@gmail.com>
2024-05-13 20:08:58 -0500

    🧪Feat (2.Comprehensions): Usando paquetes

--- 2.Comprehensions/main.py
+++ 2.Comprehensions/main.py
@@ -1,3 +1,12 @@
-import this
+from pkg.module_1 import func_1, func_2
+from pkg.module_2 import func_3, func_4
 
-# Imprimir el zend de python
+import pkg
+
+print(func_1())
+print(func_2())
+
+print(func_3())
+print(func_4())
+
+print(pkg.URL)
---
+++ 2.Comprehensions/pkg/__init__.py
@@ -0,0 +1,6 @@
+# Este __init__ se ejecuta cuando se importa el paquete
+# Aquí se pueden definir variables que se pueden usar en los módulos del paquete
+
+print("Paquete inicializado")
+
+URL = "https://restcountries.eu/rest/v2/all"
