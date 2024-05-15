d3a3811e5511c3b0a2ba2b16fa7bc99cf36ab4df
alvarado0925 <alvarado0925@gmail.com>
2024-05-14 22:59:17 -0500

    🧪Feat (2.Comprehensions): Leer archivos con python

---
+++ 2.Comprehensions/23_files.py
@@ -0,0 +1,29 @@
+path = "C:/laragon/www/cursos-python-platzi/2.Comprehensions/file.txt"
+
+print("✅ 1. Leer archivo completo:")
+file = open(path)
+print(file.read())
+file.close()  # Cerrar archivo
+
+
+print("\n✅ 2. Leer linea por linea:")
+file2 = open(path)
+print(file2.readline())  # Lee la primera linea
+print(file2.readline())  # Lee la segunda linea
+print(file2.readline())  # Lee la tercera linea
+file2.close()  # Cerrar archivo
+
+
+print("\n✅ 3. Leer todas las lineas con un for:")
+file3 = open(path)
+for line in file3:
+    print(line)
+file3.close()  # Cerrar archivo
+
+
+print("\n✅ 4. Leer todas las lineas con un with:")
+# El with se encarga de cerrar el archivo automaticamente
+file4 = open(path)
+with open(path) as file4:
+    for line in file4:
+        print(line)
